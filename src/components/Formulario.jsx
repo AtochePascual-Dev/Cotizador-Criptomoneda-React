2999943099ae86d5e171995c759c44f56d262d30
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-09 15:34:12 -1000

    [FEATURE] consultar api

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,26 @@
 import Select from "./Select"
 import { monedas } from "../data/monedas"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 const Formulario = () => {
+
+  const [criptos, setCriptos] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const URL = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
+
+      const respuesta = await fetch(URL);
+      const resultado = await respuesta.json();
+
+      const criptoMonedasAPI = resultado.Data.map((critoApi) => ({ id: critoApi.CoinInfo.Name, nombre: critoApi.CoinInfo.FullName }))
+
+      setCriptos(criptoMonedasAPI)
+    };
+
+    consultarAPI();
+  }, []);
 
 
   return (
@@ -14,7 +31,11 @@ const Formulario = () => {
         data={monedas}
       />
 
-
+      <Select
+        label="Elige tu Criptomoneda"
+        id="criptomoneda"
+        data={criptos}
+      />
     </form>
   )
 }
