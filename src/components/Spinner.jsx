d8cb23d9c63ff18f9eb74e80894363c265215865
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-09 17:01:30 -1000

    [FEATURE] snniper component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,20 @@
+import '../styles/Spinner.css';
+
+
+const Spinner = () => {
+
+  return (
+    <div className='contenedorSpinner' >
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
