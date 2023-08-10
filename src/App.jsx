82c6f7489ca64389b568e7956b1493d9c173bfa5
Atoche Pascual <jmatochepascual@gmail.com>
2023-08-09 14:58:44 -1000

    [FEATURE] heder component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
+import styled from "@emotion/styled";
+import ImagenCriptoPng from './img/imagen-criptos.png';
+
+const Contianer = styled.div`
+  width: 90%;
+  max-width: 90rem;
+  margin: 0 auto;
+  padding-top: 5rem;
+  display: grid;
+  justify-items: center;
+  gap: 4em;
+  color: white;
+  
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2,1fr);
+  }
+`;
+
+const ImagenCripto = styled.img`
+  max-width: 28rem;
+  margin-top: 2rem;
+  display: block;
+  
+  @media (min-width: 768px) {
+    max-width: 35rem;
+  }
+`;
+
+const Header = styled.h1`
+  margin-bottom: 5rem;
+  font-size: 4rem;
+  text-align:center;
+  
+  &::after{
+  content: "";
+  width: 20rem;
+  height: .6rem;
+  margin: 1rem auto 0;
+  display: block;
+  background-color: #69a4fb;
+  }
+`;
+
 function App() {
 
   return (
-    <>
-      <p>xd</p>
-    </>
+    <Contianer>
+
+      <ImagenCripto
+        src={ImagenCriptoPng}
+        alt="imagen criptomoneda"
+      />
+
+      <div>
+        <Header>Cotiza tu Criptomoneda</Header>
+      </div>
+
+    </Contianer>
   )
 }
 
